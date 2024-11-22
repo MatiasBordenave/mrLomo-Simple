868578004c56fcf308a2350c48ce73f61de55070
MatuqE <bordenavetuc@gmail.com>
2024-11-22 19:31:26 -0300

    intento de arreglo GET status 404

--- src/pages/Ventas/Ventas.jsx
+++ src/pages/Ventas/Ventas.jsx
@@ -5,7 +5,80 @@ import '../../styles/ventas.css';
 import { DETALLEVENTA_DELETE, DETALLEVENTA_DELETE_ALL, DETALLEVENTA_GET_VENTA, DETALLEVENTA_POST, FACTURA_GET_NUM, Footer, NavPrincipal, PRODUCTO_GET, TOTAL_GET_SUMA, VENTA_POST } from '../../constants/constants';
 import { TablaProducto, TablaUnProducto, ModalTicket } from "../../constants/constants"
 
-import productosData from '../../data/productos.js';
+const productosData = [
+    {
+      "id": 1,
+      "nombre": "Hamburguesa Clásica",
+      "descripcion": "Pan artesanal, carne de res, lechuga, tomate, queso y mayonesa.",
+      "precio": 1200,
+      "categoria": "Comida",
+      "stockeable": false,
+      "stock": 20
+    },
+    {
+      "id": 2,
+      "nombre": "Milanesa Completa",
+      "descripcion": "Pan francés, milanesa de pollo, queso, tomate y lechuga.",
+      "precio": 1300,
+      "categoria": "Comida",
+      "stockeable": false,
+      "stock": 15
+    },
+    {
+      "id": 3,
+      "nombre": "Gaseosa 500ml",
+      "descripcion": "Botella de gaseosa de 500ml, varios sabores disponibles.",
+      "precio": 600,
+      "categoria": "Bebida",
+      "stockeable": true,
+      "stock": 50
+    },
+    {
+      "id": 4,
+      "nombre": "Cerveza Artesanal 330ml",
+      "descripcion": "Cerveza artesanal rubia o negra, en botella de 330ml.",
+      "precio": 900,
+      "categoria": "Bebida",
+      "stockeable": false,
+      "stock": 30
+    },
+    {
+      "id": 5,
+      "nombre": "Papas Fritas",
+      "descripcion": "Porción mediana de papas fritas crocantes.",
+      "precio": 700,
+      "categoria": "Acompañamiento",
+      "stockeable": false,
+      "stock": 25
+    },
+    {
+      "id": 6,
+      "nombre": "Hamburguesa Vegana",
+      "descripcion": "Pan integral, medallón de lentejas, palta, rúcula y tomate.",
+      "precio": 1400,
+      "categoria": "Comida",
+      "stockeable": false,
+      "stock": 10
+    },
+    {
+      "id": 7,
+      "nombre": "Agua Mineral 500ml",
+      "descripcion": "Botella de agua mineral natural o con gas.",
+      "precio": 500,
+      "categoria": "Bebida",
+      "stockeable": true,
+      "stock": 40
+    },
+    {
+      "id": 8,
+      "nombre": "Sándwich de Lomito",
+      "descripcion": "Pan francés, lomito, queso, tomate, lechuga y huevo.",
+      "precio": 1500,
+      "categoria": "Comida",
+      "stockeable": false,
+      "stock": 12
+    }
+  ]
 
 
 
